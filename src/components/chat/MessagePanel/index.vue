bd5488bb131e92594591f30fc16ee1d4026c9fdf
WangScaler <WangScaler@163.com>
2021-12-08 10:06:06 +0800

    feat(message): 增加消息的时间

--- src/components/chat/MessagePanel/index.vue
+++ src/components/chat/MessagePanel/index.vue
@@ -18,13 +18,18 @@
             v-if="item.message_type === 'text'"
             class="message"
             v-html="replaceEmotionText(item.message_content)"
-          ></span>
+          >
+          </span>
           <span v-if="item.message_type === 'img'" class="emoji">
             <img
+              v-image-preview
               :src="item.message_content"
               loading="lazy"
               @click="previewImg(item)"
             />
+          </span>
+           <span :class="['time']">
+             {{ getFormatTime(item.updatedAt) }}
           </span>
         </div>
 
@@ -92,6 +97,43 @@ export default {
         this.$refs.end.scrollIntoView(true);
       });
     },
+   getFormatTime(timeStamp){
+     if(timeStamp == null){
+        return "刚刚";
+     }
+      var dateTime = new Date(timeStamp);
+      var no1new = dateTime.valueOf();
+      var year = dateTime.getFullYear();
+      var month = dateTime.getMonth() + 1;
+      var day = dateTime.getDate();
+      var hour = dateTime.getHours();
+      var minute = dateTime.getMinutes();
+      var second = dateTime.getSeconds();
+      var now = new Date();
+      var now_new = now.valueOf();
+      var milliseconds = 0;
+      var timeSpanStr;
+
+      milliseconds = now_new - no1new;
+
+      if (milliseconds <= 1000 * 60 * 1) {
+        timeSpanStr = '刚刚';
+      }else if (1000 * 60 * 1 < milliseconds && milliseconds <= 1000 * 60 * 60) {
+        timeSpanStr = Math.round((milliseconds / (1000 * 60))) + '分钟前';
+      }else if (1000 * 60 * 60 * 1 < milliseconds && milliseconds <= 1000 * 60 * 60 * 24) {
+        timeSpanStr = Math.round(milliseconds / (1000 * 60 * 60)) + '小时前';
+      }else if (1000 * 60 * 60 * 24 < milliseconds && milliseconds <= 1000 * 60 * 60 * 24 * 15) {
+        timeSpanStr = Math.round(milliseconds / (1000 * 60 * 60 * 24)) + '天前';
+      }else if (milliseconds > 1000 * 60 * 60 * 24 * 15 && year == now.getFullYear()) {
+        // timeSpanStr = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
+        timeSpanStr = year + '-' + month + '-' + day
+      }else {
+        // timeSpanStr = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
+        timeSpanStr = year + '-' + month + '-' + day
+      }
+      
+      return timeSpanStr;
+      }
   },
   created() {},
   mounted() {
@@ -168,9 +210,14 @@ export default {
             cursor: pointer;
           }
         }
+        .time {
+          font-size: 10px;
+          color: #7c7a7a;
+        }
         .name {
           font-size: 14px;
           color: #3c3c3c;
+          display:inline
         }
       }
 
